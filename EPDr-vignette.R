1f0cbdffe20b3400e8b6d24bf6dc8e5563949a65
Diego Nieto Lugilde <dnietolugilde@gmail.com>
2016-02-18 15:38:02 +0100

    Vignette update
    Update with examples for the new functions

--- EPDr-vignette.R
+++ EPDr-vignette.R
@@ -21,14 +21,29 @@ install_github("dinilu/EPDr", force=T)
 
 # Connecting to the DDBB server in diegonl.ugr.es -------------------------
 library(EPDr)
-connEPD <- connectToEPD(HOST="diegonl.ugr.es", DB="epd_ddbb", US="epdr", PW="epdrpw")
+connEPD <- connectToEPD(host="diegonl.ugr.es", database="epd_ddbb", user="epdr", password="epdrpw")
 dbListTables(connEPD)
 dbGetQuery(connEPD, "SELECT e_ FROM synevent;")
 disconnectFromEPD(connEPD)
 
+
+library(EPDr)
+connEPD <- connectToEPD(host="diegonl.ugr.es", database="epd_ddbb", user="epdr", password="epdrpw")
+c14 <- extractC14(400, connEPD)
+chron <- extractChronologies(400, connEPD)
+
+c14.clam <- c14toCLAM(c14)
+noc14.clam <- noC14toCLAM(chron$no_C14)
+
+synevent <- dbGetQuery(connEPD, "SELECT * FROM synevent WHERE e_ = 51;")
+event <- dbGetQuery(connEPD, paste("SELECT * FROM event WHERE event_ = ", synevent$event_, ";", sep=""))
+event <- merge(synevent, event, by="event_")
+event.clam <- eventtoCLAM(event)
+
+
 # Section 1 - Recalibrate chronologies ------------------------------------------
 library(EPDr)
-connEPD <- connectToEPD(HOST="diegonl.ugr.es", DB="epd_ddbb", US="epdr", PW="epdrpw")
+connEPD <- connectToEPD(host="diegonl.ugr.es", database="epd_ddbb", user="epdr", password="epdrpw")
 
 # Site with one chronologies with EXTRA data
 core4Clam(1, connEPD, get.dephts=T)
@@ -40,7 +55,7 @@ clam("4")
 
 
 # Cores with events
-eventList <- unique(as.numeric(dbGetQuery(connEPD, "SELECT e_ FROM synevent;")[,1]))
+eventList <- unique(dbGetQuery(connEPD, "SELECT e_ FROM synevent;")[,1])
 #[1]   51  141  220  457  554  571  679  806  868  874  932  966  980 1030 1031 1032
 lapply(eventList[c(4:6, 11:13, 15)], core4Clam, connEPD, get.dephts=T)
 core4Clam(1032, connEPD, get.dephts=T)
